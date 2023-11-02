9498dc78e012418ee7102fe13d62afb3e3f5d4d0
amberjabeen <ambjab465@gmail.com>
2023-11-02 01:24:40 -0700

    add new file

---
+++ script.js
@@ -0,0 +1,15 @@
+// Your JavaScript code here
+
+// This function is executed when the page has fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // You can perform various actions here, such as adding event listeners or manipulating the DOM.
+  
+    // Example: Add an event listener to a button with the ID "myButton"
+    const myButton = document.getElementById("myButton");
+  
+    myButton.addEventListener("click", function () {
+      // Code to execute when the button is clicked
+      alert("Button clicked!");
+    });
+  });
+  
